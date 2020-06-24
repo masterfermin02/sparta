21e1e4071eadb0a947af31e6f0b8f7d58f05f7be
masterfermin02 <fperdomo@intellisys.com.do>
2020-06-24 14:02:19 -0400

    First game commit

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed = 1f;
+    public float minX;
+    public float maxX;
+    public float waitingTime = 2f;
+
+    private GameObject _target;
+    private Animator _animator;
+    private Weapon _weapon;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _weapon = GetComponentInChildren<Weapon>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        UpdateTarget();
+        StartCoroutine("PatrolToTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void UpdateTarget()
+    {
+        if (_target == null) {
+            _target = new GameObject("Target");
+            _target.transform.position = new Vector2(minX, transform.position.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+            return;
+        }
+
+        if (_target.transform.position.x == minX) {
+            _target.transform.position = new Vector2(maxX, transform.position.y);
+            transform.localScale = new Vector3(1, 1, 1);
+            return;
+        }
+        
+        _target.transform.position = new Vector2(minX, transform.position.y);
+        transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private IEnumerator PatrolToTarget()
+    {
+        while(Vector2.Distance(transform.position, _target.transform.position) > 0.05f) {
+            
+            _animator.SetBool("Idle", false);
+
+            Vector2 direction = _target.transform.position - transform.position;
+            float xDirection = direction.x;
+
+            transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        Debug.Log("Target reached");
+        transform.position = new Vector2(_target.transform.position.x, transform.position.y);
+        UpdateTarget();
+        _animator.SetBool("Idle", true);
+        _animator.SetTrigger("Shoot");
+
+    
+
+        Debug.Log("Waiting for " + waitingTime + " seconds");
+        yield return new WaitForSeconds(waitingTime);
+
+        Debug.Log("Waited enough, let's update the target and move again");
+        StartCoroutine("PatrolToTarget");
+    }
+
+    void CaShoot()
+    {
+        if(_weapon != null) {
+            _weapon.Shoot();
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject shooter;
+
+    private Transform _firePoint;
+
+    void Awake()
+    {
+        _firePoint = transform.Find("FirePoint");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot()
+    {
+        if (canShoot()) {
+            GameObject myBullet = Instantiate(bulletPrefab, _firePoint.position, Quaternion.identity) as GameObject;
+            Bullet bulletComponent = myBullet.GetComponent<Bullet>();
+            bulletComponent.direction = getShooterDirection();
+        }
+
+    }
+
+    private bool canShoot() 
+    {
+        return bulletPrefab != null && _firePoint != null && shooter != null;
+    }
+
+    private Vector2 getShooterDirection() 
+    {
+        if (shooter.transform.localScale.x < 0f) {
+            return Vector2.left;
+        }
+
+        return Vector2.right;
+    }
+}
