5d066f8c1cd2dff7ccceab69bb5d7abfb278d4ba
masterfermin02 <fperdomo@intellisys.com.do>
2020-06-28 22:30:59 -0400

    Added some UI

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _player;
+
+    private SpriteRenderer _playerSpriteRenderer;
+
+    public float speedforce;
+    
+    private void Awake() {
+        _player = GetComponent<Rigidbody2D>();
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        _player.velocity = new Vector2(horizontalInput * speedforce, _player.velocity.y);
+
+        _playerSpriteRenderer.flipX = (horizontalInput < 0);
+        
+    }
+}
